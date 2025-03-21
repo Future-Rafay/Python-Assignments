07319026865af099ae397c4027462f72344940c8
Abdul Rafay <160708639+Future-Rafay@users.noreply.github.com>
2025-03-21 20:30:09 +0500

    01_expressions Completed

--- Assignment-1/01_expressions/03_feet_to_inches.py
+++ Assignment-1/01_expressions/03_feet_to_inches.py
@@ -1,5 +1,13 @@
+INCHES_IN_FOOT = 12
+
+
+def convert_to_feet(feet: float):
+    return print(f"{feet} feet = {feet * INCHES_IN_FOOT} inches")
+
+
 def main():
-    print("Delete this line and write your code here! :)")
+    feet: float = float(input("Enter The Feet To Convert To Inches :"))
+    print(convert_to_feet(feet))
 
 
 # This provided line is required at the end of
