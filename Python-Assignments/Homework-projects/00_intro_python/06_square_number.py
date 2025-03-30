269347ecefbaf989b080e555d61901c09fb5c6b2
Abdul Rafay <160708639+Future-Rafay@users.noreply.github.com>
2025-03-31 00:09:10 +0500

    Added Files Again

---
+++ Python-Assignments/Homework-projects/00_intro_python/06_square_number.py
@@ -0,0 +1,8 @@
+def main():
+    num : float =  float(input("Type a number to see its square: "))
+    print(f"{num} square is {num * num}")
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
